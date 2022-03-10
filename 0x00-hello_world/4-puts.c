bc758861b33c81bd8e3df9c143c73011f645410f
Joy-learns <adeshinaayomide12@yahoo.com>
2022-03-10 16:55:13 +0000

    task 4 well

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/* this code is to use the puts command */
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
