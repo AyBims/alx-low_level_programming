8a1c2a48a822382e133c808a79704fb5db34f3c8
Joy-learns <adeshinaayomide12@yahoo.com>
2022-03-10 14:49:33 +0000

    task 4 correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/*
+this code is to use the puts command
+*/
+ 
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
