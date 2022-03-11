9684392aba2aa249e5f665db0a3610fcf41c974e
Joy-learns <adeshinaayomide12@yahoo.com>
2022-03-11 17:43:21 +0000

    tsk6 gambling

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
 /**
  *main - Entry point
  *Return: Always 0 (Success)
  */
 int main(void)
 {
-int ch = 0;
-for (ch = 0; ch <= 9; ch++)
+int ch = '0';
+for (ch = '0'; ch <= '9'; ch++)
 putchar(ch);
 putchar('\n');
 return (0);
