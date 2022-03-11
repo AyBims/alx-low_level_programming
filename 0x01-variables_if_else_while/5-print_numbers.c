2e03997b05729bfe9d71b770810a987c021952f7
Joy-learns <adeshinaayomide12@yahoo.com>
2022-03-11 17:20:56 +0000

    tsk5 gambling

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch = 1;
+for (ch = 1; ch <= 9; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
