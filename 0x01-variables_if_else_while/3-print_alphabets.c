f898206008f55f9e1a1b6f5e630de4affe05c64c
Joy-learns <adeshinaayomide12@yahoo.com>
2022-03-11 16:13:29 +0000

    task 3 gamble

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'Z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
