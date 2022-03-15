7112bb0469449d298885d68ea05e3952c19ebb9c
Joy-learns <adeshinaayomide12@yahoo.com>
2022-03-15 19:54:35 +0000

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+$include "main.h"
+/**
+ *_abs - function that generates absolute value of a number
+ *@n: is a number
+ *Return: Always 0 (success)
+ */
+int _abs(int n)
+{
+if (n < 1)
+{
+n = -1 * n;
+_putchar('n');
+return (0);
+}
+else
+{
+_putchar(n);
+return ('0');
+}
+}
