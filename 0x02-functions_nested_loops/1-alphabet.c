09d55f6f435f0ddcc9289d3f0bc9e9439fc648e4
Joy-learns <adeshinaayomide12@yahoo.com>
2022-03-15 14:33:44 +0000

    new directory

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,2 +1,16 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ *main - entry point
+ *return: Always 0 (success)
+ */
+int main(void)
+{
+void print_alphabet(void)
+{
+ char ch  = 'a';
+for(ch = 'a'; ch >= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+return (0);
+}
 
