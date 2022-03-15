6a9c5fab5f34e07e763d0758be47e0b44f5e5c3d
Joy-learns <adeshinaayomide12@yahoo.com>
2022-03-15 14:39:12 +0000

    task 1 changes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
  *main - entry point
+ *this function prints the alphabets
  *return: Always void (success)
  */
 void print_alphabet(void)
 {
- char ch  = 'a';
-for(ch = 'a'; ch >= 'z'; ch++)
+char ch  = 'a';
+for (ch = 'a'; ch >= 'z'; ch++)
 _putchar(ch);
 _putchar('\n');
-return;
+return (void);
 }
 
