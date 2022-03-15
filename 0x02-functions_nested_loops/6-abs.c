6f6adb7f6f3929a2e47cb04014d07a243c5eacc5
Joy-learns <adeshinaayomide12@yahoo.com>
2022-03-15 19:59:36 +0000

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,19 +2,16 @@
 /**
  *_abs - function that generates absolute value of a number
  *@n: is a number
- *Return: Always 0 (success)
+ *Return: n if positive and -n if negative (success)
  */
 int _abs(int n)
 {
 if (n < 1)
 {
-n = -1 * n;
-_putchar('n');
-return (0);
+return (-n);
 }
 else
 {
-_putchar(n);
-return ('0');
+return (n);
 }
 }
