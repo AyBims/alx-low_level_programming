7e4eb9a486914926dab2c44781f1df0f9aa1523b
Joy-learns <adeshinaayomide12@yahoo.com>
2022-03-15 14:44:34 +0000

    task 1 correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *main - entry point
- *this function prints the alphabets
+ *print_alphabet: this function prints the alphabets
  *return: Always void (success)
  */
 void print_alphabet(void)
@@ -9,7 +9,7 @@ void print_alphabet(void)
 char ch  = 'a';
 for (ch = 'a'; ch >= 'z'; ch++)
 write(ch);
-wtite('\n');
+write('\n');
 return;
 }
 
