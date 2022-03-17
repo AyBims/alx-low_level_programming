b64086a62fe38d7c8196e565b04907cc3b9669b6
Joy-learns <adeshinaayomide12@yahoo.com>
2022-03-17 21:07:54 +0000

    back again

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,13 +6,12 @@
  */
 void print_numbers(void)
 {
-	int num;
+	char num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = '0'; num <= '9'; num++)
 
 	{
 		_putchar(num);
 	}
 	_putchar('\n');
-	return (0);
 }
