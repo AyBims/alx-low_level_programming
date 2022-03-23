77b0277b059bab0fc412fddc903804e80152e626
Joy-learns <adeshinaayomide12@yahoo.com>
2022-03-23 00:14:38 +0000

    updating

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - prints out the output of a string in reverse
+ * @s: string/character input
+ * Return: no output
+ */
+
+void rev_string(char *s)
+{
+        int count = 0;
+
+        while (count >= 0)
+        {
+                if (s[count] == '\0')
+                        break;
+                count++;
+
+                for (count--; count >= 0; count--)
+                        _putchar(s[count]);
+                _putchar('\n');
+        }
+}
