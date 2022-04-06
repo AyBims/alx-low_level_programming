67221554cb6b51699699f488f92ab28702e01744
Joy-learns <adeshinaayomide12@yahoo.com>
2022-04-06 21:49:22 +0000

    finishing tasks

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: argument value
+ * Return: always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", arv[i]);
+	}
+	return (0);
+}
